4b7e2a3fad60c3dbe507b1e0a82d633854c53246
xuenhan <xshe0033@student.monash.edu>
2025-10-07 15:42:58 +0800

    Imported Vegalite

---
+++ vega_lite_vis.js
@@ -0,0 +1,7 @@
+var vg_1 = "mean_household_income.vg.json";
+vegaEmbed("#chloropleth_map", vg_1).then(function(result) {
+}).catch(console.error);
+
+var vg_2 = "lineplot.vg.json";
+vegaEmbed("#lineplot", vg_2).then(function(result) {
+}).catch(console.error)
